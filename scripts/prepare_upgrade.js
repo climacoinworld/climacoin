aa60bf93cad6192ce40895b7a8a43e51a091beaa
Constantin Dogaru <constantin.dogaru@mbntech.ro>
2021-10-17 18:06:21 +0300

    indent the javascript files

--- scripts/prepare_upgrade.js
+++ scripts/prepare_upgrade.js
@@ -1,16 +1,16 @@
 // scripts/prepare_upgrade.js
 async function main() {
-  const proxyAddress = '0xb4AE58AE84aB13fd235447bD1c9F7D2545C23C88';
- 
+  const proxyAddress = "0xb4AE58AE84aB13fd235447bD1c9F7D2545C23C88";
+
   const BoxV2 = await ethers.getContractFactory("BoxV2");
   console.log("Preparing upgrade...");
   const boxV2Address = await upgrades.prepareUpgrade(proxyAddress, BoxV2);
   console.log("BoxV2 at:", boxV2Address);
 }
- 
+
 main()
   .then(() => process.exit(0))
-  .catch(error => {
+  .catch((error) => {
     console.error(error);
     process.exit(1);
   });
