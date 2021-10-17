99b084f7aff3f4064dbecdf3d358ec77b1e863b4
Constantin Dogaru <constantin.dogaru@mbntech.ro>
2021-10-17 16:15:48 +0300

    format solidity contracts

--- contracts/ClimaCoinToken.sol
+++ contracts/ClimaCoinToken.sol
@@ -6,7 +6,11 @@ import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/Initializable.sol";
 
 contract ClimaCoinToken is Initializable, ERC20Upgradeable {
-    function initialize(string memory name, string memory symbol, uint256 initialSupply) public virtual initializer {
+    function initialize(
+        string memory name,
+        string memory symbol,
+        uint256 initialSupply
+    ) public virtual initializer {
         __ERC20_init(name, symbol);
         _mint(_msgSender(), initialSupply);
     }
