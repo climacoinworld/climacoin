c7798eff57855a3970a3dcec9818b37cf70507b0
Constantin Dogaru <constantin.dogaru@mbntech.ro>
2021-10-17 15:59:17 +0300

    add climacoin token contract

---
+++ contracts/ClimaCoinToken.sol
@@ -0,0 +1,13 @@
+// contracts/ClimaCoinToken.sol
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.7.0;
+
+import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/Initializable.sol";
+
+contract ClimaCoinToken is Initializable, ERC20Upgradeable {
+    function initialize(string memory name, string memory symbol, uint256 initialSupply) public virtual initializer {
+        __ERC20_init(name, symbol);
+        _mint(_msgSender(), initialSupply);
+    }
+}
