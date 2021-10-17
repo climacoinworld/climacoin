1cfda7d3430e0516e048ce62e9c96bb91c2b7c43
Constantin Dogaru <constantin.dogaru@mbntech.ro>
2021-10-17 16:14:16 +0300

    add script for deploying climacoin token

---
+++ scripts/deploy_token.js
@@ -0,0 +1,15 @@
+// scripts/deploy_token.js
+async function main() {
+    const ClimaCoinToken = await ethers.getContractFactory("ClimaCoinToken");
+    console.log("Deploying ClimaCoinToken...");
+    const climaCoinToken = await upgrades.deployProxy(ClimaCoinToken, ['ClimaCoin Token', 'CLC', 29000000000 * 10 ** 18], { initializer: 'initialize' });
+    console.log("ClimaCoinToken deployed to:", climaCoinToken.address);
+  }
+  
+  main()
+    .then(() => process.exit(0))
+    .catch(error => {
+      console.error(error);
+      process.exit(1);
+    });
+    
