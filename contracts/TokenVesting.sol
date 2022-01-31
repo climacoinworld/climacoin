9f9446b9a551390b5a45e7b41ff5631d5c58ebb0
Constantin Dogaru <ctin.dogaru95@gmail.com>
2022-02-01 00:38:25 +0200

    fix imports

--- contracts/TokenVesting.sol
+++ contracts/TokenVesting.sol
@@ -2,9 +2,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "./IERC20.sol";
-import "./SafeERC20.sol";
-import "./SafeMath.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
 /**
  * @title TokenVesting
